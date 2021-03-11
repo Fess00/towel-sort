a79b25d5f31f07afd4121d4de618d0822aefa561
Fess00 <ss_s0l0@mail.ru>
2021-03-11 16:01:21 +0300

    completed task 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ module.exports = function towelSort (matrix) {
         return arr;
     }
     for (let i = 0; i < matrix.length; i++) {
-        console.log(matrix[i])
         for (let j = 0; j < matrix[i].length; j++) {
             if (i % 2 === 0) {
                 arr.push(matrix[i][j]);
